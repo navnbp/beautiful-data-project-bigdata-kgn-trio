0bcdcd407528ba177e3391bc74c8ea852a429a04
navnbp <navnbp@gmail.com>
2016-04-24 22:48:06 -0700

    travis build fail fix

--- src/main/java/edu/csula/datascience/acquisition/APIDataSource.java
+++ src/main/java/edu/csula/datascience/acquisition/APIDataSource.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 import java.util.List;
 import javax.net.ssl.HttpsURLConnection;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.csula.datascience.model.Configuration;
+//import edu.csula.datascience.model.Configuration;
 import edu.csula.datascience.model.Crime;
 import edu.csula.datascience.model.CrimeJSON;
 
