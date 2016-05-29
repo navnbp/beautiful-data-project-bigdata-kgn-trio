dffa68459894c30384197177a8ab235bf375e6ae
navnbp <navnbp@gmail.com>
2016-05-29 13:26:41 -0700

    added location data member

--- src/main/java/edu/csula/datascience/model/CrimeJSON.java
+++ src/main/java/edu/csula/datascience/model/CrimeJSON.java
@@ -6,7 +6,7 @@ public class CrimeJSON {
 	
 	public String arrest;
 //	public String beat;
-//	public String block;
+	public String block;
 	public String case_number;
 	public String community_area;
 	public String date;
@@ -16,15 +16,12 @@ public class CrimeJSON {
 	public String fbi_code;
 	public String id;
 //	public String iucr;
-//	public String latitude;
-	//public String location;
+	public String latitude;
 	public String location_description;
-//	public String longitude;
+	public String longitude;
 	public String primary_type;
 	public String updated_on;
 	public String ward;
-//	public String x_coordinate;
-//	public String y_coordinate;
 	public String year;
 	
 	public CrimeJSON(){}
@@ -32,11 +29,11 @@ public class CrimeJSON {
 	public CrimeJSON(String arrest, String beat, String block, String case_number, String community_area, String date,
 			String description, String district, String domestic, String fbi_code, String id, String iucr,
 			String latitude, String location_description, String longitude, String primary_type,
-			String updated_on, String ward, String x_coordinate, String y_coordinate, String year) {
+			String updated_on, String ward, String year) {
 		super();
 		this.arrest = arrest;
 //		this.beat = beat;
-//		this.block = block;
+		this.block = block;
 		this.case_number = case_number;
 		this.community_area = community_area;
 		this.date = date;
@@ -46,15 +43,15 @@ public class CrimeJSON {
 		this.fbi_code = fbi_code;
 		this.id = id;
 //		this.iucr = iucr;
-//		this.latitude = latitude;
-		//this.location = location;
+		this.latitude = latitude;
+		
 		this.location_description = location_description;
-//		this.longitude = longitude;
+		this.longitude = longitude;
 		this.primary_type = primary_type;
 		this.updated_on = updated_on;
 		this.ward = ward;
-//		this.x_coordinate = x_coordinate;
-//		this.y_coordinate = y_coordinate;
+		/*this.x_coordinate = x_coordinate;
+		this.y_coordinate = y_coordinate;*/
 		this.year = year;
 	}
 	public String getArrest() {
@@ -63,18 +60,6 @@ public class CrimeJSON {
 	public void setArrest(String arrest) {
 		this.arrest = arrest;
 	}
-	/*public String getBeat() {
-		return beat;
-	}
-	public void setBeat(String beat) {
-		this.beat = beat;
-	}
-	public String getBlock() {
-		return block;
-	}
-	public void setBlock(String block) {
-		this.block = block;
-	}*/
 	public String getCase_number() {
 		return case_number;
 	}
@@ -123,36 +108,12 @@ public class CrimeJSON {
 	public void setId(String id) {
 		this.id = id;
 	}
-	/*public String getIucr() {
-		return iucr;
-	}
-	public void setIucr(String iucr) {
-		this.iucr = iucr;
-	}
-	public String getLatitude() {
-		return latitude;
-	}
-	public void setLatitude(String latitude) {
-		this.latitude = latitude;
-	}*/
-/*	public String getLocation() {
-		return location;
-	}
-	public void setLocation(String location) {
-		this.location = location;
-	}*/
 	public String getLocation_description() {
 		return location_description;
 	}
 	public void setLocation_description(String location_description) {
 		this.location_description = location_description;
 	}
-	/*public String getLongitude() {
-		return longitude;
-	}
-	public void setLongitude(String longitude) {
-		this.longitude = longitude;
-	}*/
 	public String getPrimary_type() {
 		return primary_type;
 	}
@@ -171,22 +132,56 @@ public class CrimeJSON {
 	public void setWard(String ward) {
 		this.ward = ward;
 	}
-	/*public String getX_coordinate() {
-		return x_coordinate;
-	}
-	public void setX_coordinate(String x_coordinate) {
-		this.x_coordinate = x_coordinate;
-	}
-	public String getY_coordinate() {
-		return y_coordinate;
-	}
-	public void setY_coordinate(String y_coordinate) {
-		this.y_coordinate = y_coordinate;
-	}*/
 	public String getYear() {
 		return year;
 	}
 	public void setYear(String year) {
 		this.year = year;
 	}
+	public String getBlock() {
+		return block;
+	}
+
+	public void setBlock(String block) {
+		this.block = block;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+		public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	/*public String getX_coordinate() {
+		return x_coordinate;
+	}
+
+	public void setX_coordinate(String x_coordinate) {
+		this.x_coordinate = x_coordinate;
+	}
+
+	public String getY_coordinate() {
+		return y_coordinate;
+	}
+
+	public void setY_coordinate(String y_coordinate) {
+		this.y_coordinate = y_coordinate;
+	}*/
+	
+	
+	
+	
+	
+	
+
 }
